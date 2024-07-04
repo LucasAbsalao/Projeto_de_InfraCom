4627eb91478c229d4e463fc6e68e3737a36f7af2
Alex Carvalho <jac@cin.ufpe.br>
2024-07-04 16:03:22 -0300

    preparing to merge

--- testev/client.py
+++ testev/client.py
@@ -3,8 +3,8 @@ import socket as skt
 
 addr = ('127.0.0.1',5555)
 
-client = Udp.socketUdp("127.0.0.1", 9999, 2048)
+client = Udp.socketUdp("127.0.0.1", 9999, 1024)
 #client.send_file('files/image.jpg', addr)
 #client.send_file('files/texto.txt', addr)
-#client.send_file('files/livro.pdf', addr)
-client.send_file('files/music.mp3', addr)
+client.send_file('files/livro.pdf', addr)
+#client.send_file('files/music.mp3', addr)
